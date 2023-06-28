1bdb690641dbc1200c4c4a8ed026b7c4fc48f5cf
Giordano Campogiani <campogio@hotmail.it>
2023-06-28 22:27:12 +0200

    Table Template

--- crud.php
+++ crud.php
@@ -5,6 +5,22 @@
 	$main = new Template("dtml/admin-fixed-sidebar.html");
 	$body = new Template("dtml/datatable-card.html");
 	
+	$body->setContent("column","id");
+	$body->setContent("column","Bib");
+	
+	
+	$body->setContent("id","0");
+	$body->setContent("row","Test");
+	
+	$body->setContent("id","1");
+	$body->setContent("row","Test2");
+	
+	$body->setContent("id","2");
+	$body->setContent("row","Test3");
+	
+	
+	
+	
 	$main->setContent("body",$body->get());
 	$main ->close();
 
