d1b0027f0a9baeb54641373af8b069d92e32d505
Giordano Campogiani <campogio@hotmail.it>
2023-06-29 03:19:48 +0200

    CRUD (Read)

--- admin.php
+++ admin.php
@@ -9,8 +9,47 @@
 	$tables = getTables();
 	
 	while ($data = $tables->fetch_assoc()){
-		$main->setContent("table_name","admin.php?".$data['Tables_in_mydb']);
+		$main->setContent("table_link","admin.php?state=1&table=".$data['Tables_in_mydb']);
 		$main->setContent("table",$data['Tables_in_mydb']);
+	}
+	
+	switch ($_GET['state']){
+		case 1:
+			//handle CRUD body
+			$body = new Template("dtml/datatable-card.html");
+			
+			$columns = getTableColumns($_GET['table']);
+			
+			$columns_arr = array();
+			
+			$i=0;
+			
+			while ($data = $columns->fetch_assoc()){
+				$body->setContent("column",$data['COLUMN_NAME']);
+				$columns_arr[$i]=$data['COLUMN_NAME'];
+				$i++;
+			}
+			
+			$rows = getTableData($_GET['table']);
+			
+			while ($data = $rows->fetch_assoc()){
+				for($i=0;$i<count($columns_arr);$i++){
+					if($i==0){
+						$body->setContent("id",$data[$columns_arr[$i]]);
+					}else{
+						$body->setContent("row",$data[$columns_arr[$i]]);
+					}
+				}
+			}
+			
+			
+			
+			
+			$main->setContent("body",$body->get());
+		break;
+		
+		default:
+			break;
 	}
 	
 	
--- include/dbservice.inc.php
+++ include/dbservice.inc.php
@@ -2,10 +2,40 @@
 
     require "dbms.inc.php";
     
+    ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+    
+    
     function getTables(){
         
         global $mysqli;
         $result= $mysqli->query("SHOW TABLES");
+        return $result;
+        
+    }
+    
+    function getTableColumns($table){
+        
+        global $mysqli;
+        $stmt = $mysqli->prepare("SELECT COLUMN_NAME
+                                        FROM INFORMATION_SCHEMA.COLUMNS
+                                        WHERE TABLE_SCHEMA = 'mydb' AND TABLE_NAME = ?;");
+        
+        $stmt->bind_param("s",$table);
+        
+        $stmt->execute();
+        
+        $result = $stmt->get_result();
+        
+        return $result;
+        
+    }
+    
+    function getTableData($table){
+        
+        //Needs a whitelist of tables to make sure of no SQL Injections, it's not possible to parametrize a table name
+        
+        global $mysqli;
+        $result = $mysqli->query("SELECT * FROM ".$table);
         return $result;
         
     }
