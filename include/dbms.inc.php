ab3d25725e691b4c33032c9a682b71695291f689
Giordano Campogiani <campogio@hotmail.it>
2023-06-30 17:52:52 +0200

    Test Homepage

--- include/dbms.inc.php
+++ include/dbms.inc.php
@@ -2,9 +2,9 @@
 
 	
 	
-    //ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
+    ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 
-	$mysqli = new mysqli("localhost", "root", database:"mydb");
+	$mysqli = new mysqli("localhost", "root",password: "password", database:"mydb");
 
     if ($mysqli->connect_error) {
         echo "Not connected to DB, error: " . $mysqli->connect_error;
--- index.php
+++ index.php
@@ -12,13 +12,41 @@
 
     $main = new Template("dtml/frame-public.html");
     $body = new Template("dtml/shop-home.html");
+	$product = new Template("dtml/product.html");
+	
+	
+	for($k=0;$k<3;$k++){
+		
+		$body->setContent("category", "Category ".$k);
+		
+		for($i=0;$i<3;$i++){
+			
+			echo $i;
+			
+			$body->setContent("productName","Product ".$i);
+			$body->setContent("productPrice", $i);
+			
+			//Item is on sale?
+			if(true){
+				$body->setContent("saleRibbon",'<div class="ribbon sale">SALE</div>');
+			}
+			//Item is new?
+			if(true){
+				$body->setContent("newRibbon",'<div class="ribbon new">NEW</div>');
+			}
+			
+		}
+		
+	}
+	
+	
+	
 	
 	
 	if(isset($_SESSION['auth']['admin.php'])){
 		$main->setContent("dashboard", "<a href='admin.php'>Administration Dashboard</a>");
 	}
     $main->setContent("body", $body->get());
-
     $main->close();
 
 
